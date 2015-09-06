87d34db7538fb23c604cb939390733ec79b16d4d
Rich Hildred <rhildred@gmail.com>
2015-09-06 10:59:43 -0400

    took the carousel out of the .js and into an ng-init in the markup

--- js/singlepageui-bootstrap.js
+++ js/singlepageui-bootstrap.js
@@ -20,20 +20,6 @@ angular.module('app', ['ui.bootstrap', 'ngRoute', 'duScroll'])
     }])
 
     .controller('mainController', function($scope, $document) {
-    $scope.myInterval = 5000;
-    $scope.noWrapSlides = false;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-        var newWidth = 600 + slides.length + 1;
-        slides.push({
-            image: '//placekitten.com/' + newWidth + '/300',
-            text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-            ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-        });
-    };
-    for (var i=0; i<10; i++) {
-        $scope.addSlide();
-    }
     $scope.bAboveMenu = true;
     $scope.navClass = "navbar-static-top";
     window.onscroll = function() {
